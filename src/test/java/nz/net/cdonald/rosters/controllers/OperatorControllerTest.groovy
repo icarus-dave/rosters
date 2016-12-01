592dc6f58272044ebc9f89c40a5e2dc9570c7709
icarus-dave <davidm@cdonald.nz>
2016-12-01 15:20:48 +1300

    Improved operator validation code

--- src/test/java/nz/net/cdonald/rosters/controllers/OperatorControllerTest.groovy
+++ src/test/java/nz/net/cdonald/rosters/controllers/OperatorControllerTest.groovy
@@ -5,6 +5,7 @@ import nz.ac.auckland.morc.MorcTestBuilder
 import nz.net.cdonald.rosters.Application
 import nz.net.cdonald.rosters.domain.Operator
 import org.apache.camel.Exchange
+import org.apache.camel.ExpressionEvaluationException
 import org.apache.camel.Predicate
 import org.apache.camel.Processor
 import org.apache.camel.jsonpath.JsonPathExpression
@@ -29,6 +30,7 @@ class OperatorControllerTest extends MorcTestBuilder {
 		o1.lastName = "zyx"
 		o1.email = "foo@baz.com"
 
+
 		syncTest("Test of Populated List", "http://localhost:8080/operator")
 				.request(process { server.save(generateOperator()) })
 				.expectation(headers(header(Exchange.CONTENT_TYPE, "application/json;charset=UTF-8")),
@@ -41,6 +43,7 @@ class OperatorControllerTest extends MorcTestBuilder {
 					jsonpath(".[?(@.firstName == 'zyx')]"), jsonpath(".[?(@.lastName == 'abc')]"), jsonpath(".[?(@.email == 'baz@foo.com')]"))
 
 		def o = generateOperator("1", "2", "1@2.com")
+
 
 		syncTest("Test of GET", "http://localhost:8080/operator/123456")
 				.expectsException()
@@ -59,50 +62,68 @@ class OperatorControllerTest extends MorcTestBuilder {
 		def i
 
 		syncTest("Create and Update Operator", "http://localhost:8080/operator")
-				.request(headers(header(Exchange.HTTP_METHOD, POST())), json('{ "firstName":"rrr","lastName":"nnn","email":"abde@asdd" }'))
+				.request(headers(header(Exchange.HTTP_METHOD, POST())), json('{ "firstName":"rrr","lastName":"nnn","email":"abde@asdd.com" }'))
 				.expectation(predicate { i = new JsonPathExpression("@.id").evaluate(it); i instanceof Integer },
-				headers(header(Exchange.CONTENT_TYPE, "application/json;charset=UTF-8")))
+				jsonpath(".[?(@.version == 1)]"),headers(header(Exchange.CONTENT_TYPE, "application/json;charset=UTF-8")))
 			.addPart("http://localhost:8080/operator")
 				.request(process { it.getIn().setHeader(Exchange.HTTP_PATH, "/${i}") })
-				.expectation(jsonpath(".[?(@.lastName == 'nnn')]"), jsonpath(".[?(@.active == true)]"))
+				.expectation(jsonpath(".[?(@.lastName == 'nnn')]"), jsonpath(".[?(@.active == true)]"),jsonpath(".[?(@.version == 1)]"))
 			.addPart("http://localhost:8080/operator")
 				.request(process { it.getIn().setHeader(Exchange.HTTP_PATH, "/${i}") },
-				headers(header(Exchange.HTTP_METHOD, PUT())), process {
-					json('{ "id": ' + i + ', "firstName":"nnn","lastName":"rrr","email":"abde@asdd","active":false }').process(it) })
+				headers(header(Exchange.HTTP_METHOD, PUT())), process { json('{ "id": ' + i + ', "firstName":"nnn","lastName":"rrr","email":"abde@asdd.com","active":false,"version":1 }').process(it) })
 				.expectation(jsonpath(".[?(@.firstName == 'nnn')]"), jsonpath(".[?(@.lastName == 'rrr')]"),
-				jsonpath(".[?(@.email == 'abde@asdd')]"), jsonpath(".[?(@.active == false)]"))
+				jsonpath(".[?(@.version == 2)]"),jsonpath(".[?(@.email == 'abde@asdd.com')]"), jsonpath(".[?(@.active == false)]"))
+
 
 		syncTest("Create operator ID specified", "http://localhost:8080/operator")
-				.request(headers(header(Exchange.HTTP_METHOD, POST())), json('{ "id": 45678, "firstName":"rrr","lastName":"nnn","email":"asdf@asdf","version":555}'))
+				.request(headers(header(Exchange.HTTP_METHOD, POST())), json('{ "id": 45678, "firstName":"rrr","lastName":"nnn","email":"asdf@asdf.com","version":555}'))
 				.expectation(predicate { def j = new JsonPathExpression("@.id").evaluate(it); j instanceof Integer && j != 45678 },
-				jsonpath(".[?(@.version == 1)]"), headers(header(Exchange.CONTENT_TYPE, "application/json;charset=UTF-8")))
+				jsonpath(".[?(@.version == 1)]"),
+				//hacky way to check non-existence of element
+				predicate { try { new JsonPathExpression("@.whenCreated").evaluate(it) } catch (ExpressionEvaluationException e) { return true }; return false },
+				headers(header(Exchange.CONTENT_TYPE, "application/json;charset=UTF-8")))
+
 
 		syncTest("Update operator not known", "http://localhost:8080/operator/12345")
 				.request(headers(header(Exchange.HTTP_METHOD, PUT())))
 				.expectsException()
 				.expectation(httpStatusCode(400))
 
+
 		syncTest("Update operator mismatch", "http://localhost:8080/operator")
 				.request(process { it.getIn().setHeader(Exchange.HTTP_PATH, "/${i}") },
-				headers(header(Exchange.HTTP_METHOD, PUT())), json('{ "id":"123","firstName":"nnn","lastName":"rrr","email":"abde@asdd" }'))
+				headers(header(Exchange.HTTP_METHOD, PUT())), json('{ "id":"123","firstName":"nnn","lastName":"rrr","email":"abde@asdd.com" }'))
 				.expectsException()
 				.expectation(httpStatusCode(400))
 
+
 		syncTest("Existing email create fail", "http://localhost:8080/operator")
-				.request(headers(header(Exchange.HTTP_METHOD, POST())), json('{ "firstName":"rrr","lastName":"nnn","email":"abde@asdd" }'))
+				.request(headers(header(Exchange.HTTP_METHOD, POST())), json('{ "firstName":"rrr","lastName":"nnn","email":"abde@asdd.com" }'))
 				.expectsException()
 				.expectation(httpStatusCode(400))
+
 
 		syncTest("Existing email create fail", "http://localhost:8080/operator")
 				.request(headers(header(Exchange.HTTP_METHOD, DELETE())))
 				.expectsException()
 				.expectation(httpStatusCode(405))
 
-		/*
-			todo: teams
 
-			once done then frontend
-		 */
+		syncTest("Create operator with invalid email", "http://localhost:8080/operator")
+				.request(headers(header(Exchange.HTTP_METHOD, POST())), json('{ "firstName":"rrr","lastName":"nnn","email":"invalid" }'))
+				.expectsException()
+				.expectation(httpStatusCode(400))
+
+		def j
+
+		syncTest("Update operator with dumb version number", "http://localhost:8080/operator")
+				.request(headers(header(Exchange.HTTP_METHOD, POST())), json('{ "firstName":"rrr","lastName":"nnn","email":"test@test.com" }'))
+				.expectation(jsonpath(".[?(@.version == 1)]"),predicate { j = new JsonPathExpression("@.id").evaluate(it); j instanceof Integer })
+			.addPart("http://localhost:8080/operator")
+				.request(process { it.getIn().setHeader(Exchange.HTTP_PATH, "/${j}") }, headers(header(Exchange.HTTP_METHOD, PUT())),
+					process { json('{ "id": ' + j + ', "firstName":"nnn","lastName":"rrr","email":"test@test.com","active":false,"version":9999 }').process(it) })
+				.expectsException()
+				.expectation(httpStatusCode(409))
 
 	}
 
