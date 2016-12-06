54ea0dd282a3249d822baebb62bd6d5a275ef19a
icarus-dave <davidm@cdonald.nz>
2016-12-07 09:32:12 +1300

    added support for CORS in local dev environment

--- src/main/java/nz/net/cdonald/rosters/Application.groovy
+++ src/main/java/nz/net/cdonald/rosters/Application.groovy
@@ -5,6 +5,11 @@ import com.avaje.ebean.springsupport.factory.EbeanServerFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
@@ -27,3 +32,16 @@ public class Application {
 }
 
 
+@Configuration
+@Profile("local")
+public class LocalDevConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
