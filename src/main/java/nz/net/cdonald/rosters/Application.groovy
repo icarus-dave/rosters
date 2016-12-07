23df05fa6ccb4939d9adfe13d252df714f005fda
icarus-dave <davidm@cdonald.net.nz>
2016-12-07 15:19:09 +1300

    prefixed /api to controllers to support easy reverse proxying in the future.

--- src/main/java/nz/net/cdonald/rosters/Application.groovy
+++ src/main/java/nz/net/cdonald/rosters/Application.groovy
@@ -4,12 +4,15 @@ import com.avaje.ebean.config.ServerConfig;
 import com.avaje.ebean.springsupport.factory.EbeanServerFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class Application {
@@ -43,5 +46,23 @@ public class LocalDevConfiguration {
                 registry.addMapping("/**");
             }
         };
+    }
+}
+
+//borrowed from: http://stackoverflow.com/questions/28006501/how-to-specify-prefix-for-all-controllers-in-spring-boot
+@Configuration
+public class DispatcherServletCustomConfiguration {
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean dispatcherServletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                dispatcherServlet(), "/api/*");
+        registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
+        return registration;
     }
 }
