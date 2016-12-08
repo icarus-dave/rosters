e3a8abf490330699425e5e77dfcd36f0b0189062
icarus-dave <davidm@cdonald.net.nz>
2016-12-08 20:05:59 +1300

    improved ebean deployment support

--- src/main/java/nz/net/cdonald/rosters/controllers/ErrorControllerAdvice.groovy
+++ src/main/java/nz/net/cdonald/rosters/controllers/ErrorControllerAdvice.groovy
@@ -1,5 +1,7 @@
 package nz.net.cdonald.rosters.controllers
 
+import ch.qos.logback.classic.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.HttpRequestMethodNotSupportedException
@@ -8,10 +10,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
 import javax.persistence.OptimisticLockException
+import javax.persistence.PersistenceException
 import javax.servlet.http.HttpServletRequest
 
 @ControllerAdvice
 class ErrorControllerAdvice {
+
+	static final Logger logger = LoggerFactory.getLogger(ErrorControllerAdvice.class)
+
 	@ExceptionHandler([IllegalArgumentException.class, MethodArgumentTypeMismatchException.class])
 	public ResponseEntity handleBadRequests(Exception e) throws IOException {
 		def res = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "Invalid parameter provided: ${e.message}")
@@ -28,5 +34,12 @@ class ErrorControllerAdvice {
 	public ResponseEntity handleOptimisticLockException(HttpServletRequest request) throws IOException {
 		def res = new ErrorResponse(HttpStatus.CONFLICT.value(), "Conflict in updating object. Try GETing again and then updating")
 		return new ResponseEntity(res, HttpStatus.CONFLICT)
+	}
+
+	@ExceptionHandler(PersistenceException.class)
+	public ResponseEntity handlePersistenceException(Exception e) throws IOException {
+		def res = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Server error working with database")
+		logger.error("Returning 500 to user", e)
+		return new ResponseEntity(res, HttpStatus.INTERNAL_SERVER_ERROR)
 	}
 }
