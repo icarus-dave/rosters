cacfa3ca789525e6710e1c1900d7591f1c7b956d
icarus-dave <davidm@cdonald.net.nz>
2016-12-10 19:47:47 +1300

    Added support for frontend web-config retrieval.

---
+++ src/main/java/nz/net/cdonald/rosters/controllers/WebConfigController.groovy
@@ -0,0 +1,36 @@
+package nz.net.cdonald.rosters.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.AbstractEnvironment
+import org.springframework.core.env.Environment
+import org.springframework.core.env.MapPropertySource
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/*
+	Any config property prefixed with "webconfig." will be returned
+	to the caller sans the prefix. The intention being to enable the front
+	end to query the backend for environment variables (and easily cacheable).
+ */
+
+@RestController
+@RequestMapping("/api/webconfig")
+class WebConfigController {
+
+	@Autowired
+	Environment environment
+
+	@RequestMapping
+	public Map<String, String> config() {
+		def webConfig = [:]
+		(environment as AbstractEnvironment).getPropertySources().each { ps ->
+			if (!(ps instanceof MapPropertySource)) return
+			if (!(ps.getSource() instanceof Map)) return
+			ps.getSource().each { k,v ->
+				if (k.startsWith("frontend.")) webConfig.put(k - "frontend.",environment.getProperty(k) as String)
+			}
+		}
+		return webConfig
+	}
+
+}
