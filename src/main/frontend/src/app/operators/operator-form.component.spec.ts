327c5997602dfc30a608c88261e3dfd68b9eefbf
icarus-dave <davidm@cdonald.net.nz>
2016-12-28 15:52:18 +1300

    completed tests for operator form

--- src/main/frontend/src/app/operators/operator-form.component.spec.ts
+++ src/main/frontend/src/app/operators/operator-form.component.spec.ts
@@ -7,6 +7,7 @@ import { By }              from '@angular/platform-browser';
 import { NgbModule, NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { Operator } from './shared/operator.model';
 import { OperatorService } from './shared/operator.service';
 import { OperatorFormComponent } from './operator-form.component';
 import { InMemoryDataService } from '../in-memory-data-service';
@@ -36,37 +37,147 @@ describe('OperatorFormComponentTest', () => {
     fixture.detectChanges();
   });
 
-  it("submit new operator", () => {
+  it("submit new operator",(done) => { inject([OperatorService],(service) => {
+    //create with service and close modal
+    let params = {id: undefined, firstName:"abc",lastName:"def",active:true,email:"foo@baz.com"};
+    let operator = new Operator(params);
 
+    spyOn(service,"create").and.callFake((o) => Promise.resolve(o) );
+    spyOn(comp.activeModal,"close").and.callFake((o) => Promise.resolve(o) );
+    comp.form.patchValue(params);
+
+    comp.onSubmit().then(() => {
+      //normally this would set the ID but not for a unit test
+      expect(service.create).toHaveBeenCalledWith(operator);
+      expect(comp.activeModal.close).toHaveBeenCalledWith(operator);
+      done();
+    });
+    })();
   });
 
-  it("submit operator error", () => {
+  it("submit operator error", (done) => { inject([OperatorService],(service) => {
+    //create with service and close modal
+    let params = {id: undefined, firstName:"abc",lastName:"def",active:true,email:"foo@baz.com"};
+    let operator = new Operator(params);
 
+    spyOn(service,"create").and.callFake((o) => Promise.reject("some server error") );
+    spyOn(comp.activeModal,"close");
+    comp.form.patchValue(params);
+
+    comp.onSubmit().then(() => {
+      expect(service.create).toHaveBeenCalledWith(operator);
+      expect(comp.activeModal.close).toHaveBeenCalledTimes(0);
+      expect(comp.errorMessage).toContain("some server error");
+      done();
+    })
+    })();
   });
 
-  it("submit existing operator", () => {
+  it("submit existing operator", (done) => { inject([OperatorService],(service) => {
+    let params = {id: 123, firstName:"abc",lastName:"def",active:true,email:"foo@baz.com"};
+    let operator = new Operator(params);
 
+    spyOn(service,"update").and.callFake((o) => Promise.resolve(o) );
+    spyOn(comp.activeModal,"close").and.callFake((o) => Promise.resolve(o) );
+    comp.form.patchValue(params);
+
+    comp.onSubmit().then(() => {
+      expect(service.update).toHaveBeenCalledWith(operator);
+      expect(comp.activeModal.close).toHaveBeenCalledWith(operator);
+      done();
+    });
+    })();
   });
 
-  it("submit existing operator error",() => {
+  it("submit existing operator error", (done) => { inject([OperatorService],(service) => {
+    //create with service and close modal
+    let params = {id: 123, firstName:"abc",lastName:"def",active:true,email:"foo@baz.com"};
+    let operator = new Operator(params);
 
-  });
+    spyOn(service,"update").and.callFake((o) => Promise.reject("some server error") );
+    spyOn(comp.activeModal,"close");
+    comp.form.patchValue(params);
 
-  it ("update: should fill in values",() => {
-
+    comp.onSubmit().then(() => {
+      expect(service.update).toHaveBeenCalledWith(operator);
+      expect(comp.activeModal.close).toHaveBeenCalledTimes(0);
+      expect(comp.errorMessage).toContain("some server error");
+      done();
+    })
+    })();
   });
 
   it('create: should fill in values', ()  => {
-
     spyOn(comp, 'onSubmit');
 
     let emailEl = fixture.nativeElement.querySelector('input[formControlName=email]')
     let firstNameEl = fixture.nativeElement.querySelector('input[formControlName=firstName]')
     let lastNameEl = fixture.nativeElement.querySelector('input[formControlName=lastName]')
     let activeEl = fixture.nativeElement.querySelector('input[formControlName=active]')
+    let submitEl = fixture.nativeElement.querySelector('button[type=submit]')
+    let errorEl = fixture.nativeElement.querySelector('#error-message');
 
-    console.log(emailEl.classList.contains("ng-invalid"));
+    //first check the invalid ones will be marked as invalid
+    expect(emailEl.classList).toContain("ng-invalid");
+    expect(firstNameEl.classList).toContain("ng-invalid");
+    expect(lastNameEl.classList).toContain("ng-invalid");
+    expect(submitEl.disabled).toEqual(true);
+    expect(activeEl.checked).toEqual(false);
+    expect(errorEl).toBeNull();
 
+    //now update the vlues
+    emailEl.value = "foo";
+    firstNameEl.value = "first name";
+    lastNameEl.value = "last name";
+    activeEl.checked = true;
+
+    emailEl.dispatchEvent(new Event('input'));
+    firstNameEl.dispatchEvent(new Event('input'));
+    lastNameEl.dispatchEvent(new Event('input'));
+    activeEl.dispatchEvent(new Event('change'));
+     
+    fixture.detectChanges();
+
+    //check their CSS class is now as expected
+    expect(emailEl.classList).toContain("ng-invalid");
+    expect(emailEl.classList).toContain("ng-dirty");
+    expect(firstNameEl.classList).toContain("ng-valid");
+    expect(lastNameEl.classList).toContain("ng-valid");
+    expect(submitEl.disabled).toEqual(true);
+
+    //fix email and check submit button works
+    emailEl.value = "foo@baz.com";
+    emailEl.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(emailEl.classList).toContain("ng-valid");
+    expect(submitEl.disabled).toEqual(false);
+
+    //now we submit
+    fixture.nativeElement.querySelector('button[type=submit]').click();
+
+    fixture.detectChanges();
+
+    expect(comp.onSubmit).toHaveBeenCalled();
+
+    //now check everything is set correctly
+    expect(comp.form.value.id).toBeUndefined();
+    expect(comp.form.value.firstName).toEqual("first name");
+    expect(comp.form.value.lastName).toEqual("last name");
+    expect(comp.form.value.email).toEqual("foo@baz.com");
+    expect(comp.form.value.active).toEqual(true);
+  });
+
+  it("create: error message on submit",() => {
+    spyOn(comp, 'onSubmit').and.callFake(() => comp.errorMessage = "asdf");
+
+    let emailEl = fixture.nativeElement.querySelector('input[formControlName=email]')
+    let firstNameEl = fixture.nativeElement.querySelector('input[formControlName=firstName]')
+    let lastNameEl = fixture.nativeElement.querySelector('input[formControlName=lastName]')
+    let activeEl = fixture.nativeElement.querySelector('input[formControlName=active]')
+    let submitEl = fixture.nativeElement.querySelector('button[type=submit]')
+    let errorEl = fixture.nativeElement.querySelector('#error-message');
+
+    //now update the vlues
     emailEl.value = "foo@baz.com";
     firstNameEl.value = "first name";
     lastNameEl.value = "last name";
@@ -74,35 +185,58 @@ describe('OperatorFormComponentTest', () => {
 
     emailEl.dispatchEvent(new Event('input'));
     firstNameEl.dispatchEvent(new Event('input'));
-    //lastNameEl.dispatchEvent(new Event('input'));
+    lastNameEl.dispatchEvent(new Event('input'));
     activeEl.dispatchEvent(new Event('change'));
      
-
-    //todo: on select class 
-
     fixture.detectChanges();
-    console.log(emailEl);
 
-    console.log(fixture.nativeElement.querySelector('button[type=submit]').disabled);
+    expect(errorEl).toBeNull();
 
+    //now we submit
     fixture.nativeElement.querySelector('button[type=submit]').click();
 
     fixture.detectChanges();
-    console.log(comp.form.value);
+    errorEl = fixture.nativeElement.querySelector('#error-message');
+    //check values remain
+    expect(firstNameEl.value).toEqual("first name");
     
-    expect(comp.onSubmit).toHaveBeenCalled();
+    expect(errorEl).toBeTruthy();
+    expect(errorEl.textContent).toContain("asdf");
+  });
 
+  it ("update: should fill in values",() => {
+    spyOn(comp,'onSubmit');
+    comp.operator = new Operator({'id':123,'firstName':"asdf",lastName:"jkl",email:"asdf@jkl.com",active:true});
 
-    //test submit button is not working
+    fixture.detectChanges();
 
-    //set email
-    //set firstname
-    //set lastname
-    //set active
-    //click save
-    //confirm operator service called
-    //get operator back from modal service and expect values
+    let idEl = fixture.nativeElement.querySelector('input[formControlName=id]')
+    let emailEl = fixture.nativeElement.querySelector('input[formControlName=email]')
+    let firstNameEl = fixture.nativeElement.querySelector('input[formControlName=firstName]')
+    let lastNameEl = fixture.nativeElement.querySelector('input[formControlName=lastName]')
+    let activeEl = fixture.nativeElement.querySelector('input[formControlName=active]')
+    let submitEl = fixture.nativeElement.querySelector('button[type=submit]')
+    let errorEl = fixture.nativeElement.querySelector('#error-message');   
 
+    expect(idEl.value).toEqual('123');
+    expect(emailEl.classList).toContain("ng-valid");
+    expect(emailEl.value).toEqual("asdf@jkl.com");
+    expect(firstNameEl.classList).toContain("ng-valid");
+    expect(firstNameEl.value).toEqual("asdf");
+    expect(lastNameEl.classList).toContain("ng-valid");
+    expect(lastNameEl.value).toEqual("jkl");
+    expect(submitEl.disabled).toEqual(false);
+    expect(activeEl.checked).toEqual(true);
+    expect(errorEl).toBeNull();
+
+    emailEl.value = "foo";
+    emailEl.dispatchEvent(new Event('input'));
+
+    fixture.detectChanges();
+
+    expect(emailEl.classList).toContain("ng-invalid");
+    expect(submitEl.disabled).toEqual(true);
 
   });
+
 });
