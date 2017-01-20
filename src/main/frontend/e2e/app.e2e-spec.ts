132375af72e480a2d93890492eda2261555692b9
icarus-dave <davidm@cdonald.net.nz>
2017-01-20 14:21:19 +1300

    added tests for authentication login

--- src/main/frontend/e2e/app.e2e-spec.ts
+++ src/main/frontend/e2e/app.e2e-spec.ts
@@ -1,14 +1,77 @@
-import { FrontPage } from './app.po';
+import { browser, element, by, ExpectedConditions } from 'protractor';
 
 describe('front App', function() {
-  let page: FrontPage;
 
-  beforeEach(() => {
-    page = new FrontPage();
+  afterEach(function() {
+      browser.executeScript('window.sessionStorage.clear();');
+      browser.executeScript('window.localStorage.clear();');
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+  it('redirect with unvalidated email',() => {
+    browser.get("/")
+    browser.waitForAngular()
+
+    browser.ignoreSynchronization = true;
+    
+    browser.wait(() => browser.isElementPresent(by.css(".auth0-lock-submit")),10000)
+    
+    let emailEl = element(by.css(".auth0-lock-input-email input"))
+    let passwordEl = element(by.css(".auth0-lock-input-password input"))
+
+    emailEl.sendKeys("unverified-rosters@cdonald.nz")
+    passwordEl.sendKeys("password")
+    
+    element(by.css('.auth0-lock-submit')).click()
+
+    browser.wait(() => browser.getCurrentUrl().then(url => /login/.test(url)),10000)
+
+    browser.ignoreSynchronization = false;
+
+    let errorEl = element(by.css(".alert.alert-danger"))
+    expect(errorEl.getText()).toContain("Please verify your email before logging in.")
+
+    let loginAgain = element(by.css(".alert.alert-info"))
+    expect(loginAgain.getText()).toContain("login again")
   });
+
+  it('redirect with valid email',() => {
+    browser.get("/")
+    browser.waitForAngular()
+
+    browser.ignoreSynchronization = true;
+    
+    browser.wait(() => browser.isElementPresent(by.css(".auth0-lock-submit")),10000)
+    
+    let emailEl = element(by.css(".auth0-lock-input-email input"))
+    let passwordEl = element(by.css(".auth0-lock-input-password input"))
+
+    emailEl.sendKeys("foo@cdonald.nz")
+    passwordEl.sendKeys("password")
+    
+    element(by.css('.auth0-lock-submit')).click()
+
+    browser.wait(() => browser.getCurrentUrl().then(url => /operators/.test(url)),10000)
+
+    expect(element(by.css(".navbar-brand")).getText()).toEqual("Rosters")
+
+  });
+
+  //we've got an SSO session with auth0 and don't need to enter details again
+  it('redirect with get parameter',() => {
+    browser.get('/operators?foo=baz');
+    browser.waitForAngular()
+
+    browser.ignoreSynchronization = true;
+
+    browser.wait(ExpectedConditions.textToBePresentInElement(element(by.css(".auth0-lock-social-button-text")),'foo@cdonald.nz'),10000)
+
+    element(by.css(".auth0-lock-social-button")).click()
+
+    browser.wait(() => browser.getCurrentUrl().then(url => /operators\?foo=baz/.test(url)),10000)
+
+    expect(element(by.css(".navbar-brand")).getText()).toEqual("Rosters")
+
+  });
+
+
 });
